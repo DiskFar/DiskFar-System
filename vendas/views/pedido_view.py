415b51507657275786e235a02d1590f051f50b54
emersonsoaresdasilva <emerson.soares@aluno.faculdadeimpacta.com.br>
2022-03-28 22:12:16 -0300

    Integra a Twilio

--- vendas/views/pedido_view.py
+++ vendas/views/pedido_view.py
@@ -2,17 +2,19 @@ import os
 import csv
 import mercadopago
 from datetime import datetime
-from django.http import FileResponse, HttpResponse
-from django.contrib.messages.api import success
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView
-from django.contrib import messages
+from twilio.rest import Client
 from django.db.models import Q
+from django.http import Http404
+from django.contrib import messages
+from django.contrib.messages.api import success
+from django.http import FileResponse, HttpResponse
+from vendas.forms import FormPedido, FormPedidoItem
+from django.views.generic import ListView, DetailView
+from twilio.base.exceptions import TwilioRestException
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from vendas.forms import FormPedido, FormPedidoItem
 from vendas.models import Pedido, PedidoItem, Funcionario
-from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
 
 class PedidoListView(LoginRequiredMixin, ListView):
     model = Pedido
@@ -46,6 +48,35 @@ def gerar_pagamento(pedido):
     preference_response = sdk.preference().create(preference_data)
     pedido.link_pagamento = preference_response['response']['init_point']
     Pedido.objects.filter(id=pedido.id).update(link_pagamento=pedido.link_pagamento)
+    return pedido.link_pagamento
+
+def envia_mensagem_sms(request, pedido, mensagem):
+    account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+    try:
+        client.messages.create(
+                                messaging_service_sid=os.environ.get('TWILIO_MESSAGING_SERVICE_SID'),
+                                body=f'{mensagem}',
+                                to=f'+55{pedido.cliente.celular}'
+                            )
+        messages.add_message(request, messages.ERROR, f'SMS enviado com sucesso, para o número: {pedido.cliente.celular}', extra_tags='success')
+    except TwilioRestException:
+        messages.add_message(request, messages.ERROR, f'Não foi possível enviar o SMS, verifique o número cadastrado do cliente: {pedido.cliente.celular}', extra_tags='danger')
+
+def envia_mensagem_whatsapp(request, pedido, mensagem):
+    account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+    try:
+        client.messages.create(
+                                from_=os.environ.get('TWILIO_FROM_WHATSAPP'),
+                                body=f'{mensagem}',
+                                to=f'whatsapp:+55{pedido.cliente.celular}'
+                            )
+        messages.add_message(request, messages.ERROR, f'WhatsApp enviado com sucesso, para o número: {pedido.cliente.celular}', extra_tags='success')
+    except TwilioRestException:
+        messages.add_message(request, messages.ERROR, f'Não foi possível enviar o WhatsApp, verifique o número cadastrado do cliente: {pedido.cliente.celular}', extra_tags='danger')
 
 @login_required
 def adicionar_pedido(request):
@@ -94,6 +125,18 @@ def alterar_pedido(request, id):
             pedido = Pedido.objects.get(id=id)
             gerar_pagamento(pedido)
             messages.add_message(request, messages.SUCCESS, 'Pedido alterado!', extra_tags='success')
+            if form_pedido.cleaned_data['status'] == 'R':
+                mensagem = f'Olá {pedido.cliente.nome}, tudo bem? ☺️\n\nStatus do pedido: *{pedido.get_status_display()}*!\n\nValor total: {pedido.get_total():.2f}\n\nObrigado por comprar conosco!\n\nCaso deseje realizar o pagamento online, segue link: {pedido.link_pagamento}\n\n*Observação*: Encaminhar o comprovante de pagamento para evitar cobrança no local de entrega!\n\nAtenciosamente,\nDiskFar - Drogaria e Farmácia 💙'
+                envia_mensagem_whatsapp(request, Pedido.objects.get(id=id), mensagem)
+                envia_mensagem_sms(request, Pedido.objects.get(id=id), mensagem.replace('*', ''))
+            elif form_pedido.cleaned_data['status'] == 'A':
+                mensagem = f'Olá {pedido.cliente.nome},\n\nStatus do pedido: *{pedido.get_status_display()}* 🏍️\n\nSeu pedido está a caminho, obrigado pela preferência.\n\nAtenciosamente,\nDiskFar - Drogaria e Farmácia 💙'
+                envia_mensagem_whatsapp(request, Pedido.objects.get(id=id), mensagem)
+                envia_mensagem_sms(request, Pedido.objects.get(id=id), mensagem.replace('*', ''))
+            elif form_pedido.cleaned_data['status'] == 'E':
+                mensagem = f'Olá {pedido.cliente.nome},\n\nStatus do pedido: *{pedido.get_status_display()}* 🎉\n\nSeu pedido foi entregue, obrigado pela preferência.\n\nQualquer dúvida ou reclamação, estamos à disposição 😊\n\nAtenciosamente,\nDiskFar - Drogaria e Farmácia 💙'
+                envia_mensagem_whatsapp(request, Pedido.objects.get(id=id), mensagem)
+                envia_mensagem_sms(request, Pedido.objects.get(id=id), mensagem.replace('*', ''))
             return redirect(f'/pedidos/alterar/{pedido.id}')
         else:
             messages.add_message(request, messages.ERROR, 'Erro no formulário, tente novamente!', extra_tags='danger')
